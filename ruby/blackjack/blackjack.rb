33bdd047c0b4e1ca5f0a6a1aeeeb3e192e29e37b
Raul Luca <rc_luca@yahoo.com>
2017-01-29 18:12:06 -0500

    Added deal_to_dealer method and test.

--- ruby/blackjack/blackjack.rb
+++ ruby/blackjack/blackjack.rb
@@ -3,13 +3,21 @@ require_relative 'hand'
 
 class Blackjack
   attr_accessor :player_hand
+  attr_accessor :dealer_hand
 
   def initialize
     @deck = Deck.new
   end
+
   def deal_to_player
     @player_hand = Hand.new
     @player_hand.add_card(@deck.deal_card)
     @player_hand.add_card(@deck.deal_card)
+  end
+
+  def deal_to_dealer
+    @dealer_hand = Hand.new
+    @dealer_hand.add_card(@deck.deal_card)
+    @dealer_hand.add_card(@deck.deal_card)
   end
 end
--- ruby/blackjack/tests/blackjacktest.rb
+++ ruby/blackjack/tests/blackjacktest.rb
@@ -10,4 +10,9 @@ class BlackjackTest < Test::Unit::TestCase
     @blackjack.deal_to_player
     assert(@blackjack.player_hand.cards.size == 2)
   end
+
+    def test_given_a_dealer_is_dealt_a_hand_then_hand_has_two_cards
+    @blackjack.deal_to_dealer
+    assert(@blackjack.dealer_hand.cards.size == 2)
+  end
 end
