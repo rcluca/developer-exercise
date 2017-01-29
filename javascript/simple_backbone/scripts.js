40821fc37f510f59d150c8a2b58508145c9c56d3
Raul Luca <rc_luca@yahoo.com>
2017-01-29 12:38:29 -0500

    Fetched quotes and displayed in table.

--- javascript/simple_backbone/scripts.js
+++ javascript/simple_backbone/scripts.js
@@ -2,11 +2,19 @@ const Quotes = Backbone.Collection.extend({
   url: 'https://gist.githubusercontent.com/anonymous/8f61a8733ed7fa41c4ea/raw/1e90fd2741bb6310582e3822f59927eb535f6c73/quotes.json'
 });
 
-var items = [
-        {name:"Item 1"},
-        {name:"Item 2"},
-        {name:"Item 3"}
-];
+const View = Backbone.View.extend({
+        el: '#target',
+        template: _.template($("#quotesList").html()),
+        initialize: function () {
+            this.coll = new Quotes()
+            this.listenTo(this.coll, 'reset', this.render);
+            
+            this.coll.fetch({ success: () => { this.render() ; } });
+        },
+        render: function () {
+            this.$el.html(this.template({ quotes: this.coll.toJSON() }));
+            return this;
+        }
+    });
 
-const template = $("#quotesList").html();
-$("#target").html(_.template(template)({items:items}));
+const view = new View();
