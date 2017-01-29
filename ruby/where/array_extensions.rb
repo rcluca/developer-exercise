0e4fadd2c94cf7a4aa7d2d43303e344d5a3109f1
Raul Luca <rc_luca@yahoo.com>
2017-01-29 16:32:58 -0500

    Wrote passing functionality for strings.

--- ruby/where/array_extensions.rb
+++ ruby/where/array_extensions.rb
@@ -1,5 +1,13 @@
 class Array
-    def where(clause)
-        self
+    def where(args)
+        filteredItems = []
+        args.each do |key, value|
+            self.each do |arrItem|
+                if (arrItem[key].index(value))
+                    filteredItems << arrItem
+                end
+            end
+        end
+        filteredItems
     end
 end
