b4c3c63d0d1891a8a397b8895fe6222e8274bd9f
Raul Luca <rc_luca@yahoo.com>
2017-01-29 16:56:44 -0500

    Implemented functionality for numeric values and multiple args.
    Refactored "where" function to more efficiently process args, decreasing best time complexity from O(n * a) to O(n).

--- ruby/where/array_extensions.rb
+++ ruby/where/array_extensions.rb
@@ -1,11 +1,23 @@
 class Array
     def where(args)
         filteredItems = []
-        args.each do |key, value|
-            self.each do |arrItem|
-                if (arrItem[key].index(value))
-                    filteredItems << arrItem
+        self.each do |arrItem|
+            should_include = true
+            args.each do |key, value|
+                if value.is_a? Numeric
+                    if (arrItem[key] != value)
+                        should_include = false
+                        break
+                    end
+                else
+                    if (arrItem[key].index(value).nil?)
+                        should_include = false
+                        break
+                    end
                 end
+            end
+            if (should_include)
+                filteredItems << arrItem
             end
         end
         filteredItems
