dd60e17931693ad0dcbea4e29bee7e014292db57
Raul Luca <rc_luca@yahoo.com>
2017-01-29 19:01:50 -0500

    Implemented dealer_card_shown and tests.

--- ruby/blackjack/blackjack.rb
+++ ruby/blackjack/blackjack.rb
@@ -7,18 +7,26 @@ class Blackjack
   attr_accessor :dealer
 
   def initialize
-    @deck = Deck.new
-    @player = Person.new
-    @dealer = Person.new
+      @deck = Deck.new
+      @player = Person.new
+      @dealer = Person.new
   end
 
   def deal_to_player
-    @player.add_card_to_hand(@deck.deal_card)
-    @player.add_card_to_hand(@deck.deal_card)
+      @player.add_card_to_hand(@deck.deal_card)
+      @player.add_card_to_hand(@deck.deal_card)
   end
 
   def deal_to_dealer
-    @dealer.add_card_to_hand(@deck.deal_card)
-    @dealer.add_card_to_hand(@deck.deal_card)
+      @dealer.add_card_to_hand(@deck.deal_card)
+      @dealer.add_card_to_hand(@deck.deal_card)
+  end
+
+  def dealer_card_shown
+      if (@dealer.hand.cards.size >= 2)
+          card_shown = @dealer.hand.cards[1]
+      else
+          raise "Dealer has not been dealt a hand yet."
+      end    
   end
 end
--- ruby/blackjack/tests/blackjacktest.rb
+++ ruby/blackjack/tests/blackjacktest.rb
@@ -2,17 +2,28 @@ require 'test/unit'
 require_relative '../blackjack'
 
 class BlackjackTest < Test::Unit::TestCase
-  def setup
-    @blackjack = Blackjack.new
-  end
+    def setup
+        @blackjack = Blackjack.new
+    end
   
-  def test_given_a_player_is_dealt_a_hand_then_hand_has_two_cards
-    @blackjack.deal_to_player
-    assert(@blackjack.player.hand.cards.size == 2)
-  end
+    def test_given_a_player_is_dealt_a_hand_then_hand_has_two_cards
+        @blackjack.deal_to_player
+        assert(@blackjack.player.hand.cards.size == 2)
+    end
 
     def test_given_a_dealer_is_dealt_a_hand_then_hand_has_two_cards
-    @blackjack.deal_to_dealer
-    assert(@blackjack.dealer.hand.cards.size == 2)
-  end
+        @blackjack.deal_to_dealer
+        assert(@blackjack.dealer.hand.cards.size == 2)
+    end
+
+    def test_given_dealer_has_been_dealt_hand_then_second_card_dealt_is_shown
+        @blackjack.deal_to_dealer
+        assert(@blackjack.dealer_card_shown == @blackjack.dealer.hand.cards[1])
+    end
+
+    def test_given_dealer_has_not_been_dealt_hand_then_exception_is_raised
+        assert_raise do
+            @blackjack.dealer_card_shown
+        end
+    end
 end
